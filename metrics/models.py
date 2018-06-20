b2fe67463de4a90c0b893b242228a6eaf702a205
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-06-20 16:21:49 -0500

    Initial Commit

--- metrics/models.py
+++ metrics/models.py
@@ -7,12 +7,12 @@ from django.utils import timezone
 class Metric(TimeStampedModel):
     name = models.CharField(max_length=50)
     max_score = models.PositiveIntegerField(default=10)
-    student = models.ForeignKey(Student)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
 
 class Score(TimeStampedModel):
-    metric = models.ForeignKey(Metric)
-    teacher = models.ForeignKey(Teacher)
+    metric = models.ForeignKey(Metric, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     time = models.PositiveIntegerField(default=4)
     score = models.PositiveIntegerField()
     date = models.DateField(default=timezone.now)
