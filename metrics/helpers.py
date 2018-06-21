07f2fddbfb975d8b66125e4682d7b1c0af0bbf27
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-06-21 16:39:17 -0500

    Starting work on Analytics

--- metrics/helpers.py
+++ metrics/helpers.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 
 def get_week_score_average(start_week, duration, all_scores):
@@ -11,35 +11,11 @@ def get_week_score_average(start_week, duration, all_scores):
 
 
 def get_week_range(week_number, duration, all_scores):
-    start_date = None
-    if week_number == 1:
-        start_date = datetime.strptime('Dec 3 2017', '%b %d %Y')
-    elif week_number == 2:
-        start_date = datetime.strptime('Dec 10 2017', '%b %d %Y')
-    elif week_number == 3:
-        start_date = datetime.strptime('Dec 31 2017', '%b %d %Y')
-    elif week_number == 4:
-        start_date = datetime.strptime('Jan 7 2018', '%b %d %Y')
-    elif week_number == 5:
-        start_date = datetime.strptime('Jan 14 2018', '%b %d %Y')
-    elif week_number == 6:
-        start_date = datetime.strptime('Jan 21 2018', '%b %d %Y')
-    elif week_number == 7:
-        start_date = datetime.strptime('Jan 28 2018', '%b %d %Y')
-    elif week_number == 8:
-        start_date = datetime.strptime('Mar 4 2018', '%b %d %Y')
-    elif week_number == 9:
-        start_date = datetime.strptime('Mar 11 2018', '%b %d %Y')
-    elif week_number == 10:
-        start_date = datetime.strptime('Mar 18 2018', '%b %d %Y')
-    elif week_number == 11:
-        start_date = datetime.strptime('Apr 1 2018', '%b %d %Y')
-    elif week_number == 12:
-        start_date = datetime.strptime('Apr 8 2018', '%b %d %Y')
-    elif week_number == 13:
-        start_date = datetime.strptime('Apr 15 2018', '%b %d %Y')
-    elif week_number == 14:
-        start_date = datetime.strptime('Apr 22 2018', '%b %d %Y')
+    start_date_dictionary = [date(2017, 12, 3), date(2017, 12, 10), date(2017, 12, 31), date(2018, 1, 7), date(2018, 1, 14),
+                             date(2018, 1, 21), date(2018, 1, 28), date(2018, 2, 4), date(2018, 2, 11), date(2018, 2, 18),
+                             date(2018, 2, 25), date(2018, 3, 4), date(2018, 3, 11), date(2018, 3, 18), date(2018, 4, 1),
+                             date(2018, 4, 8), date(2018, 4, 15), date(2018, 4, 22)]
+    start_date = start_date_dictionary[week_number-1]
     end_date = start_date + timedelta(days=duration*7)
     reports = []
     for report in all_scores:
