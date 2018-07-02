886e20d9b2d9182832a77c86142ce248fbdfd722
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-07-02 17:02:07 -0500

    Creating users to test database

--- Coach/urls.py
+++ Coach/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url('home/', views.all_student_view, name='Home Page')
+    url('home/', views.all_student_view, name='Home Page'),
+    url('setup/', views.setup_view, name='Setup')
 ]
--- Coach/views.py
+++ Coach/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from student.models import AcademicCoach, Student, School, Parent
+from datetime import date
 
 
-def all_student_view(request):
-    names = ['Adam', 'Beau', 'Emma', 'Mike','Nicole', 'Steven', 'Tommy']
+def setup_view(request):
+    already_setup = False
+    if already_setup:
+        return redirect('/marni/home')
+    else:
+        marni = AcademicCoach(name='Marni Pasch', username='marni', state='Florida', email='joshua.o.stafford@vanderbilt.edu', phone_number='225-304-2611')
+        marni.save()
+        bullis = School(name='Bullis School', calendar_link='https://www.bullis.org/page.cfm?p=845',
+                        website_link='https://www.bullis.org/page.cfm?p=1', state='Maryland')
+        bullis.save()
+        rob_bday = date(1995, 8, 12)
+        rob = Student(name='Rob Trone', username='robtrone', academic_coach=marni, phone_number='301-704-7703',
+                      email='robert.j.trone@vanderbilt.edu', zoom_link='https://zoom.us/j/570189575',
+                      grades_link='https://www.powerschool.com/', birthday=rob_bday, school=bullis)
+        rob.save()
+        return redirect('/marni/home')
+
+
+def all_student_view(request, user_username):
+    academic_coach = AcademicCoach.objects.get(username=user_username)
+    names = []
+    for student in academic_coach.student_set:
+        names.append(student.name)
     return render(request, 'coach/homepage.html', {'students': names, 'hidden': False})
