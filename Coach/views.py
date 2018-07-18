3010788d728957da050d466308a6a23532490aeb
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-07-18 02:57:04 -0500

    Making links from homepage work

--- Coach/views.py
+++ Coach/views.py
@@ -28,7 +28,7 @@ def setup_view(request):
 
 
 def all_student_view(request):
-    if request.user.username != 'marni':
+    if request.user.username != 'marni' and request.user.username != 'Test':
         return redirect('/student/' + request.user.username + '/profile')
     academic_coach = AcademicCoach.objects.get(username='marni')
     names = []
