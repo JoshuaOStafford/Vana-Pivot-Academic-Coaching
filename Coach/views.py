616cfcab9c93fe85c6f2fb856c888baf22742819
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-07-18 03:19:11 -0500

    Making links from homepage work

--- Coach/views.py
+++ Coach/views.py
@@ -20,8 +20,8 @@ def start_view(request):
 def setup_view(request):
     ready = True
     if ready:
-        student = Student(name='Test1', username='Test1', academic_coach=AcademicCoach.objects.get(username='Test'), birthday=date.today(), school=School.objects.get(name='No School Entered'), email='blank@gdfs.eig')
-        student.save()
+        coach = AcademicCoach(name='Test', username='Test', email='asdf@asdg.asd', phone_number='0')
+        coach.save()
         return redirect('/coach/home')
     else:
         return redirect('/coach/home')
