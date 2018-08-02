d0ba02e054559c46af21e23873ec99fc105b019d
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2018-08-01 23:59:47 -0500

    fixing bug with track grades chart

--- Vana18/settings.py
+++ Vana18/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-552+yf4#3c!f0o!c$6yz0oaq#knyxp&ss_tvavr4!0amt1-m='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True    
 
 ALLOWED_HOSTS = ['vana18.herokuapp.com',
                  'www.vanalearning.com',
