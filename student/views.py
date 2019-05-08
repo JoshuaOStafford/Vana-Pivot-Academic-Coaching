1c2abcdcf13e7d68d3922302dcb647bcdb60394e
Joshua Stafford <joshua.o.stafford@vanderbilt.edu>
2019-05-08 00:22:53 -0500

    Edit grades

--- student/views.py
+++ student/views.py
@@ -57,7 +57,7 @@ def track_grades_view(request, username):
                 grade_submission.save()
     if request.is_ajax():
         session_number = request.GET.get('sessionNumber', None)
-        session_number = int(session_number)+1
+        session_number = int(session_number)
         previous_grades = []
         subject_names = []
         date_for_session = None
